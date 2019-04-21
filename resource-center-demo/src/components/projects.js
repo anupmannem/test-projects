11114af7db57280c527f83eb7c246684725a269a
anupmannem <anupmannem@gmail.com>
2019-04-22 01:38:39 +0530

    projects page tabs setup

--- resource-center-demo/src/components/projects.js
+++ resource-center-demo/src/components/projects.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
+import { Tabs, Tab } from 'react-mdl'
 
 class Projects extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { activeTab: 0 };
+  }
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return <div><h1>This is React</h1></div>
+    } else if (this.state.activeTab === 1) {
+      return <div><h1>This is Node.js</h1></div>
+    } else if (this.state.activeTab === 2) {
+      return <div><h1>This is Javascript</h1></div>
+    } else if (this.state.activeTab === 3) {
+      return <div><h1>This is HTML/CSS</h1></div>
+    }
+  }
+  
   render() {
     return (
-      <div><h1>Projects page</h1></div>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+          <Tab>React</Tab>
+          <Tab>Node/Express</Tab>
+          <Tab>Javascript</Tab>
+          <Tab>HTML/CSS</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          {this.toggleCategories()}
+        </section>
+      </div>
     )
   }
 }
