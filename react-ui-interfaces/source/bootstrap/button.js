bf1c621323d421729c9c716f9017df6a5eb8c383
anupmannem <anupmannem@gmail.com>
2019-04-23 19:56:28 +0530

    using props with spread operator

--- react-ui-interfaces/source/bootstrap/button.js
+++ react-ui-interfaces/source/bootstrap/button.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default class Button extends React.Component {
   render() {
     return(
-      <a className={this.props.className} href={this.props.href} >
-        Discover things.
-      </a>
+      <a {...this.props}>Discover things.</a>
     )
   }
 }
