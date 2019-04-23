d799629e1f63278bba66f39b2fa43b0e48e7c3bd
anupmannem <anupmannem@gmail.com>
2019-04-23 22:57:00 +0530

    jumbtron component added

--- react-ui-interfaces/source/app.js
+++ react-ui-interfaces/source/app.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Button from './bootstrap/button'
+import Jumbotron from './bootstrap/jumbotron'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div
-        className="jumbotron jumbotron-fluid text-xs-center"
-        style={{ backgroundImage: 'url(img/debug_1.jpeg)', color: 'white' }}>
-        <div className="container">
-          <h1>Building React User Interfaces</h1>
-          <p>with bootstrap and sass</p>
-          <p><Button className="-primary" href="#" /></p>
-          <p><Button className="-danger-outline -sm" /></p>
-        </div>        
-        <Button className="-primary-outline -lg -block" disabled />
-      </div>
+      <Jumbotron className="-fluid text-xs-center"
+      style={{ backgroundImage: 'url(img/debug_1.jpeg)', color: 'white' }}>
+      <div className="container">
+        <h1>Building React User Interfaces</h1>
+        <p>with bootstrap and sass</p>
+        <p><Button className="-primary" href="#" /></p>
+        <p><Button className="-danger-outline -sm" /></p>
+      </div>        
+      <Button className="-primary-outline -lg -block" disabled />
+      </Jumbotron>
+
     );
   }
 }
